f962266edba9169ed7fc1b6b6e3a131c5741a440
Kemunto Gift <giftoyugi@gmail.com>
2022-10-26 15:04:25 +0300

    added body component and added color to it

---
+++ src/components/Body.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class Body extends Component {
+  render() {
+    return (
+      <div class="bodyclass">
+        <h1>TODO LIST</h1>
+        <i>
+          “Subtracting from your list of priorities is as important as adding to
+          it.”
+        </i>
+      </div>
+    );
+  }
+}
