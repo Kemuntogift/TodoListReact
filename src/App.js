c943827c6d933b34cee889e210913bdee60ae017
Kemunto Gift <giftoyugi@gmail.com>
2022-10-27 20:09:41 +0300

    Debug form logic and add edit sub

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
 import Body from "./components/BodyFolder/Body";
 import Form from "./components/Form/Form";
+import Todo from "./components/Todo/TodoList"
 import "./App.css";
+
 function App() {
   return (
     <div className="App">
       <Body/>
       <Form/>
+
     </div>
   );
 }
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,14 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+// import React, { useState} from 'react';
 import "./form.css";
 
-export default function Form() {
+export default function Form(props) {
+  const [input, setInput] = useState(props.edit ? props.edit.value : "");
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    props.onSubmit({
+      text: input,
+    });
+    setInput("");
+  };
   return (
     <div class="formc">
       <div>
         <h3>Add Task</h3>
       </div>
       <div class="inputdiv">
-        <input type="text" class="todo-input"></input>
+        <form onSubmit={handleSubmit} class="todo-form">
+          {props.edit ? (
+            <>
+              <input
+                type="text"
+                placeholder="Update/Edit"
+                value={input}
+                class="todo-edit"
+                onChange={handleChange}
+              ></input>
+              <button onClick={handleChange} class="todo-button-edit">
+                Update
+              </button>
+            </>
+          ) : (
+            <>
+              <input
+                type="text"
+                placeholder="Add a Task"
+                value={input}
+                class="todo-input"
+                onChange={handleChange}
+              ></input>
+
+              <button onClick={handleSubmit} class="todo-button">
+                Submit
+              </button>
+            </>
+          )}
+        </form>
         <div class="select">
           <select name="priority" class="filter-todo">
             <option value="all">Select Priority</option>
@@ -17,7 +61,6 @@ export default function Form() {
             <option value="high">High</option>
           </select>
         </div>
-        <button>Submit</button>
       </div>
     </div>
   );
